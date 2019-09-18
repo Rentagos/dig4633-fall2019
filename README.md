601029b6841c6c51cbb03abe6f92c05905dabede
Rentagos <37207246+Rentagos@users.noreply.github.com>
2019-09-18 15:43:26 -0400

    In Class 9/18

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ As you complete each lab, you should provide a description of your features and 
 ## VR Lab: 00
 
 I have tried the Oculus Quest in lab in which I played Virtual Virtual Reality, Job Simulator, the Vader Immortal: Episode 1, and Oculus First Contact. I enjoyed the games as they didn't have many movement which would cause motion sickness.
+
+In Class 9/18
+I started learning how to use some of the already made VR scripts from the farmland. However, I'm still having difficulties figuring out how to use it.
---
+++ in-class/Skills Test/Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    public GameObject loadingImage;
+    public GameObject SettingScreen;
+
+    public void PlayGame()
+    {
+        loadingImage.SetActive(false);
+        Application.LoadLevel(1);
+    }
+
+    public void Settings()
+    {
+        if (SettingScreen.activeInHierarchy)
+        {
+            SettingScreen.SetActive(false);
+        }
+
+        else SettingScreen.SetActive(true);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
